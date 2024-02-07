e15bd33d863e926da21af689b188319229cfaf96
Abhinay S. Bhadauria <knowledgepoint887@gmail.com>
2024-02-07 22:38:56 +0530

    Added JWT create and verify logic in service layer

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-
+const UserService = require('./services/user-service');
 
 const app = express();
 
@@ -17,6 +17,10 @@ const prepareAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`Server Started on Port: ${PORT}`);
 
+
+        const service = new UserService();
+        const newToken = service.createToken({ email: 'abcd@gmail.com', id: 1 });
+        console.log("new token is", newToken);
     });
 }
 
